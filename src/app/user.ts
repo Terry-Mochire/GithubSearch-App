16916cf77274580ad0520c83555c88ccef9c8485
Terry <terrybmochire@gmail.com>
2022-04-15 15:32:59 +0300

    Create user class.

---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+        
+    constructor(
+        public userName:string,
+        public userProfilePicture: any, 
+        public userBio: string,
+        public userNumberOfPublicRepos: Number,
+        public userGithubProfileLink: any
+         ){
+        
+    }
+}
