b4dde35c96e28e8603d80a17aaff4d2fef7c449a
Terry <terrybmochire@gmail.com>
2022-04-19 22:09:28 +0300

    Hide API key.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   user_URL : `https://api.github.com/users`,
   userRepositories_URL: "https://api.github.com/search/repositories?q=",
-  access_token: "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"
+  access_token: "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"
 
 };
 
