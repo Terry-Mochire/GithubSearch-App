f05a3b9725290a8623d126c070b0a610559db74e
Terry <terrybmochire@gmail.com>
2022-04-25 16:19:17 +0300

    Modify gitignore file.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    user_URL : `https://api.github.com/users`,
+    userRepositories_URL: "https://api.github.com/search/repositories?q=",
+    access_token: "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"
+  
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    user_URL : `https://api.github.com/users`,
+    userRepositories_URL: "https://api.github.com/search/repositories?q=",
+    access_token: "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"
+  
+  };
